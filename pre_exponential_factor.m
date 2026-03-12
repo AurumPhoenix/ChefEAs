0040fdadf186fe8f943281bb1afff90cf7da92b7
AurumPhoenix <ivanov.a@hotmail.com>
2026-03-12 18:31:07 -0400

    Updated display lines
    Updated display lines to better reflect the value assigned to newly created variables.

--- pre_exponential_factor.m
+++ pre_exponential_factor.m
@@ -6,11 +6,11 @@ function k0 = pre_exponential_factor(t_const)
     end
     if exist('R','var') ~= 1 %Universal gas constant
         R = 8.3145; %J/(mol*K)
-        disp('[pre_exponential_factor.m]: R not found in workspace, generating new variable w/ value 296K.')
+        disp('[pre_exponential_factor.m]: R not found in workspace, generating new variable w/ value 8.3145.')
     end
     if exist('E_a','var') ~= 1 %Activation energy
         E_a = 40000; %J/mol
-        disp('[pre_exponential_factor.m]: E_a not found in workspace, generating new variable w/ value 296K.')
+        disp('[pre_exponential_factor.m]: E_a not found in workspace, generating new variable w/ value 40000.')
     end
     k0 = (1/t_const)*exp(-E_a/(R*T_peak));
 end
--- rate_constant.m
+++ rate_constant.m
@@ -1,11 +1,11 @@
 function K = rate_constant(T,t_const)
     if exist('R','var') ~= 1 %Universal gas constant
         R = 8.3145; %J/(mol*K)
-        disp('[rate_constant.m]: R not found in workspace, generating new variable w/ value 296K.')
+        disp('[rate_constant.m]: R not found in workspace, generating new variable w/ value 8.3145.')
     end
     if exist('E_a','var') ~= 1 %Activation energy
         E_a = 40000; %J/mol
-        disp('[rate_constant.m]: E_a not found in workspace, generating new variable w/ value 296K.')
+        disp('[rate_constant.m]: E_a not found in workspace, generating new variable w/ value 40000.')
     end
     k0 = pre_exponential_factor(t_const);
     K = k0*exp(-E_a/(R*T));
