d30be25580e6196cfcc3c21393adf1acb43a0b41
AurumPhoenix <ivanov.a@hotmail.com>
2026-03-12 18:09:31 -0400

    Add files via upload

---
+++ rate_constant.m
@@ -0,0 +1,12 @@
+function K = rate_constant(T,t_const)
+    if exist('R','var') ~= 1 %Universal gas constant
+        R = 8.3145; %J/(mol*K)
+        disp('[rate_constant.m]: R not found in workspace, generating new variable w/ value 296K.')
+    end
+    if exist('E_a','var') ~= 1 %Activation energy
+        E_a = 40000; %J/mol
+        disp('[rate_constant.m]: T_peak not found in workspace, generating new variable w/ value 296K.')
+    end
+    k0 = pre_exponential_factor(t_const);
+    K = k0*exp(-E_a/(R*T));
+end
